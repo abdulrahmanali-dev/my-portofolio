89be55ffe61f2611e4a67e8a326f86d4db7df43a
Abdulrahman Ali <abdulrahman.ali6666@gmail.com>
2023-10-16 11:05:19 +0300

    added new project

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -1,4 +1,5 @@
 import project2Img from "../images/project-2.webp";
+import project3Img from "../images/project-3.webp";
 import project5Img from "../images/project-5.webp";
 import project6Img from "../images/project-6.webp";
 export const projects = [
@@ -13,17 +14,17 @@ export const projects = [
   {
     name: "Poll2Know",
     desc: "All poll2know Frontend Created By Me",
-    tech: ["HTML", "JS", "Bootstrap", "SCSS"],
+    tech: ["HTML", "JS", "Bootstrap", "SCSS" ,'jQuery'],
     imgUrl: project6Img,
     link: "https://poll2know.com/en/",
     source: "https://github.com/abdulrahmanali-dev/poll2know.com_frontend",
   },
   {
-    name: "Schoolizer",
-    desc: "Schoolizer.com Home Page Clone",
-    tech: ["HTML", "JS", "Bootstrap", "SCSS"],
-    imgUrl: project2Img,
-    link: "https://schoolizer-v2.netlify.app",
-    source: "https://github.com/abdulrahmanali-dev/schoolizer.com_frontend",
+    name: "Real Estate",
+    desc: "Real Estate Website",
+    tech: ["HTML", "JS", "CSS"],
+    imgUrl: project3Img,
+    link: "https://real-essstate.netlify.app",
+    source: "https://github.com/abdulrahmanali-dev/real-estate-website",
   },
 ];
